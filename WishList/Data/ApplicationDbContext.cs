4f54fbde0f2892d67d61b8537945ca25573492cd
Kyler Davis <kyler.davis@maritzcx.com>
2018-10-19 17:20:23 -0600

    Update WishList/Data/ApplicationDbContext.cs

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using WishList.Models;
 using Microsoft.EntityFrameworkCore;
 namespace WishList.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<Item> Items { get; set; }
+
         public ApplicationDbContext(DbContextOptions options): base(options)
         {
+
         }
     }
 }
