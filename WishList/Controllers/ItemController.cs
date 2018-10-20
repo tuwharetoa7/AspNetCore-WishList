2fdc230ba11f91e559261fa7d6992bd05241de2a
Kyler Davis <kyler.davis@maritzcx.com>
2018-10-19 18:12:27 -0600

    Update WishList/Controllers/ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -36,8 +36,8 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
         [HttpDelete]
-        public IActionResult Delete(int Id){
-            var item = _context.Items.FirstOrDefault(e => e.Id == Id);
+        public IActionResult Delete(int id){
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
